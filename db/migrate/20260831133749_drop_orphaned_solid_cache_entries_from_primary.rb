110d18741760f8c3108d4ac66c6b34d6c60b4360
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2026-08-31 17:51:05 -0300

    Reapply "chore: drop `solid_cache_entries` table from main db and update schema definition"
    This reverts commit bbed911793e8b2f03ec46d067f19d4ed02cc45d6.

--- db/migrate/20260831133749_drop_orphaned_solid_cache_entries_from_primary.rb
+++ db/migrate/20260831133749_drop_orphaned_solid_cache_entries_from_primary.rb
@@ -1,6 +1,6 @@
-class CreateSolidCacheEntries < ActiveRecord::Migration[8.1]
+class DropOrphanedSolidCacheEntriesFromPrimary < ActiveRecord::Migration[8.1]
   def change
-    create_table :solid_cache_entries do |t|
+    drop_table :solid_cache_entries do |t|
       t.binary :key, null: false, limit: 1024
       t.binary :value, null: false, limit: 512.megabytes
       t.datetime :created_at, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_26_194028) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_31_133749) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -29,16 +29,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_26_194028) do
     t.datetime "slot_start", null: false
     t.datetime "updated_at", null: false
     t.index ["slot_start"], name: "index_bookings_on_slot_start", unique: true
-  end
-
-  create_table "solid_cache_entries", force: :cascade do |t|
-    t.integer "byte_size", null: false
-    t.datetime "created_at", null: false
-    t.binary "key", null: false
-    t.bigint "key_hash", null: false
-    t.binary "value", null: false
-    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
-    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
-    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 end
