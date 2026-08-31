dbeb4f580ea3126cb2731839b124ac31e35235db
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2026-08-31 17:18:34 -0300

    refactor: remove `Api::ProfileController` and associated specs

--- app/controllers/api/profile_controller.rb
+++
@@ -1,9 +0,0 @@
-module Api
-  class ProfileController < BaseController
-    rate_limit to: 60, within: 1.minute, by: -> { rate_limit_identifier }, only: :show
-
-    def show
-      render json: UseCases::GetProfileUseCase.new.execute, status: :ok
-    end
-  end
-end
